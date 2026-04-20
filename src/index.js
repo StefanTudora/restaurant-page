f7a10a467a1d92cbd3a93e00e70f28be67e8f164
Stefan Tudora <24649875+StefanTudora@users.noreply.github.com>
2026-04-20 21:01:52 +0300

    Save progress

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import barberLogoSource from "../assets/barber.svg";
+import barberBackground from "../assets/background.jpg"
+import "./style.css";
+
+console.log("Hello, there!");
+
+/*
+ *  Add the image to the header
+ */
+function addBarberLogo() {
+    // Get the header holding the buttons
+    const headerElem = document.querySelector("header");
+
+    // Create the element holding the image
+    const barberLogo = document.createElement("img");
+    barberLogo.src = barberLogoSource;
+
+    // Append in front the navbar
+    headerElem.prepend(barberLogo);
+}
+
+function addBackground() {
+    const contentPage = document.querySelectorAll("content");
+    contentPage.forEach(node => {
+        const backgroundElem = document.createElement("img");
+        backgroundElem.src = barberBackground;
+    });
+}
+
+addBarberLogo();
